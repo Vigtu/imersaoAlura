236edfd8ed649d073bf208bd4f949c9b02b2cdb3
VICTOR CORREA GOMES <112295415+Vigtu@users.noreply.github.com>
2022-09-05 14:50:08 -0300

    Add files via upload

---
+++ calculadoraDeMedias.js
@@ -0,0 +1,10 @@
+var nome = "Victor"
+
+var primeiraNota = 9
+var segundaNota = 7
+var terceiraNota = 8
+var quartaNota = 6
+
+var notaFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4
+var notaFixada = notaFinal.toFixed(0)
+console.log("Sua nota final é " + notaFixada)
